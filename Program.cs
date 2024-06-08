132c881947135f50040168ee80aed54acde93487
Val-in <valeriia.vp1@yandex.ru>
2024-06-08 14:17:32 +0800

    Fix 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Net;
+﻿using static Final_project_class_1.PickPointDelivery;
 
 namespace Final_project_class_1
 {
@@ -7,159 +6,150 @@ namespace Final_project_class_1
     {
         public static void Main(string[] args)
         {
-            var homeDelivery = new HomeDelivery("123 Main St.", "Doe", "Jane", "+156202011", "Mr. Mike", DateTime.Now.AddHours(3));
-            var pickPointDelivery = new PickPointDelivery("Wu Mall", "Doe", "Jack", "Mr. Andrew", DayOfWeek.Wednesday, DateTime.Now.AddHours(1));
-            var shopDelivery = new ShopDelivery("Main Shop", "Doe", "James", "Mr. Carl", DayOfWeek.Wednesday, DateTime.Now.AddHours(0.5));
+            var recipient1 = new RecipientInfo("Doe", "Jane", "+156202011");
+            var recipient2 = new RecipientInfo("Doe", "Jack", "+156202012");
+            var recipient3 = new RecipientInfo("Doe", "James", "+156202013");
+
+            var homeDelivery = new HomeDelivery("123 Main St.", recipient1, "Mr. Mike", DayOfWeek.Friday, DateTime.Now.AddHours(3));
+            var pickPointDelivery = new PickPointDelivery("Wu Mall", recipient2, DayOfWeek.Wednesday, DateTime.Now.AddHours(1));
+            var shopDelivery = new ShopDelivery("Main Shop", recipient3, DayOfWeek.Wednesday, DateTime.Now.AddHours(0.5));
 
             var order1 = new Order<HomeDelivery, object>(homeDelivery, 534120, "Workbook");
             var order2 = new Order<PickPointDelivery, object>(pickPointDelivery, 534121, "Notebook");
             var order3 = new Order<ShopDelivery, object>(shopDelivery, 534122, "Pen");
 
-            order1.DisplayAddress();
+            order1.OrderInfo();
             order1.DeliveryProcess();
             Console.WriteLine();
 
-            order2.DisplayAddress();
+            order2.OrderInfo();
             order2.DeliveryProcess();
             Console.WriteLine();
 
-            order3.DisplayAddress();
+            order3.OrderInfo();
             order3.DeliveryProcess();
             Console.ReadKey();
 
         }
     }
-    public abstract class Delivery
+
+    //класс для описания товара
+    public class Product
     {
-        public string Address;
+        public string[] ProductName;
+
+        public Product()
+        {
+
+        }
+
+    }
+
+    public class RecipientInfo
+    {
         public string LastName;
         public string FirstName;
         public string PhoneNumber;
 
+        public RecipientInfo(string lastName, string firstName, string phoneNumber)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            PhoneNumber = phoneNumber;
+        }
+
+    }
+
+    public abstract class Delivery
+    {
+        public string Address;
+        public DateTime DeliveryTime;
+        public DayOfWeek DeliveryDayOfWeek;
+        public RecipientInfo Recipient;
+
         //Метод Deliveryman выводит сообщение о доставке
-        public abstract void Deliveryman();
-        public abstract void Pickpoint();
-        public abstract void DeliveryToShop();
+        public abstract void DeliveryInfo();
+
     }
 
     public class HomeDelivery : Delivery
     {
         public string CourierName { get; set; }
-        public DateTime DeliveryTime { get; set; }
 
-        public HomeDelivery(string address, string lastName, string firstName, string phoneNumber, string courierName, DateTime deliveryTime) //конструктор
+        public HomeDelivery(string address, RecipientInfo recipient, string courierName, DayOfWeek deliveryDayOfWeek, DateTime deliveryTime) //конструктор
         {
             Address = address;
-            LastName = lastName;
-            FirstName = firstName;
-            PhoneNumber = phoneNumber;
+            Recipient = recipient;
             CourierName = courierName;
+            DeliveryDayOfWeek = deliveryDayOfWeek;
             DeliveryTime = deliveryTime;
-
         }
 
-        public override void Deliveryman()
+        public override void DeliveryInfo()
         {
-            Console.WriteLine($"Delivery by {CourierName} for {LastName} {FirstName} to address {Address} at {DeliveryTime}. In a case call the number {PhoneNumber}");
+            Console.WriteLine($"Delivery by {CourierName} for {Recipient.LastName} {Recipient.FirstName} to address {Address} on {DeliveryDayOfWeek} at {DeliveryTime}. In a case call the number {Recipient.PhoneNumber}");
         }
-        public override void Pickpoint()
-        {
-            // Реализация не требуется, оставим пустой
-        }
-        public override void DeliveryToShop()
-        {
 
-        }
     }
 
     public class PickPointDelivery : Delivery
     {
-        public string CourierName { get; set; }
-        public string PickPointAddress { get; set; }
-        public DayOfWeek DeliveryDayOfWeek { get; set; }
-        public DateTime DeliveryTime { get; set; }
-
-        public PickPointDelivery(string address, string lastName, string firstName, string courierName, DayOfWeek deliveryDayOfWeek, DateTime deliveryTime) //конструктор
+        public PickPointDelivery(string address, RecipientInfo recipient, DayOfWeek deliveryDayOfWeek, DateTime deliveryTime) //конструктор
         {
-            PickPointAddress = address;
-            LastName = lastName;
-            FirstName = firstName;
-            CourierName = courierName;
+            Address = address;
+            Recipient = recipient;
             DeliveryDayOfWeek = deliveryDayOfWeek;
             DeliveryTime = deliveryTime;
         }
 
-        public override void Pickpoint()
-        {
-            Console.WriteLine($"Delivery by {CourierName} for {LastName} {FirstName} to pick point {PickPointAddress} on {DeliveryDayOfWeek} at {DeliveryTime}.");
-        }
-        public override void Deliveryman()
-        {
-            // Реализация не требуется, оставим пустой
-        }
-        public override void DeliveryToShop()
-        {
 
-        }
-    }
-
-    public class ShopDelivery : Delivery
-    {
-        public string CourierName { get; set; }
-        public string ShopAddress { get; set; }
-        public DayOfWeek DeliveryDayOfWeek { get; set; }
-        public DateTime DeliveryTime { get; set; }
-        public ShopDelivery(string address, string lastName, string firstName, string courierName, DayOfWeek deliveryDayOfWeek, DateTime deliveryTime) //конструктор
+        public override void DeliveryInfo()
         {
-            ShopAddress = address;
-            LastName = lastName;
-            FirstName = firstName;
-            CourierName = courierName;
-            DeliveryDayOfWeek = deliveryDayOfWeek;
-            DeliveryTime = deliveryTime;
+            Console.WriteLine($"Delivery for {Recipient.LastName} {Recipient.FirstName} to pick point {Address} on {DeliveryDayOfWeek} at {DeliveryTime}.");
         }
 
-        public override void DeliveryToShop()
+
+        public class ShopDelivery : Delivery
         {
-            Console.WriteLine($"Delivery by {CourierName} for {LastName} {FirstName} to pick point {ShopAddress} on {DeliveryDayOfWeek} at {DeliveryTime}.");
-        }
-        public override void Deliveryman()
-        {
+            public ShopDelivery(string address, RecipientInfo recipient, DayOfWeek deliveryDayOfWeek, DateTime deliveryTime) //конструктор
+            {
+                Address = address;
+                Recipient = recipient;
+                DeliveryDayOfWeek = deliveryDayOfWeek;
+                DeliveryTime = deliveryTime;
+            }
 
-        }
-        public override void Pickpoint()
-        {
-
-        }
-
-    }
-
-    public class Order<TDelivery, TStruct> where TDelivery : Delivery //какая еще раз тут логика?
-    {
-        public TDelivery Delivery;
-
-        public int Number;
-
-        public string Description;
-
-        public Order(TDelivery delivery, int number, string description)
-        {
-            Delivery = delivery;
-            Number = number;
-            Description = description; // где выводится description?
+            public override void DeliveryInfo()
+            {
+                Console.WriteLine($"Delivery for {Recipient.LastName} {Recipient.FirstName} to pick point {Address} on {DeliveryDayOfWeek} at {DeliveryTime}.");
+            }
         }
 
-        public void DisplayAddress()
+        public class Order<TDelivery, TStruct> where TDelivery : Delivery
         {
-            Console.WriteLine($"Order {Number}: {Delivery.Address}");
-        }
+            public TDelivery Delivery;
 
-        public void DeliveryProcess()
-        {
-            Console.WriteLine($"Processing delivery for order {Number}...");
-            Delivery.Deliveryman();
-            Delivery.Pickpoint();
-            Delivery.DeliveryToShop();
+            public int Number;
+
+            public string Description;
+
+            public Order(TDelivery delivery, int number, string description)
+            {
+                Delivery = delivery;
+                Number = number;
+                Description = description;
+            }
+
+            public void OrderInfo()
+            {
+                Console.WriteLine($"Order {Number}: {Delivery.Address}. About delivery: {Description}");
+            }
+
+            public void DeliveryProcess()
+            {
+                Console.WriteLine($"Processing delivery for order {Number}...");
+                Delivery.DeliveryInfo();
+            }
         }
     }
 }
