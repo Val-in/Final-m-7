bd43493c04576886f9fe9ea4455436640b0cef6d
Val-in <valeriia.vp1@yandex.ru>
2024-06-12 16:42:48 +0800

    final commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using static Final_project_class_1.PickPointDelivery;
+﻿using System;
+using System.Runtime.InteropServices;
+using static Final_project_class_1.PickPointDelivery;
 
 namespace Final_project_class_1
 {
@@ -18,9 +20,9 @@ namespace Final_project_class_1
             var product2 = new Product("Notebook", 1);
             var product3 = new Product("Pen", 5);
 
-            var order1 = new Order<HomeDelivery, object>(homeDelivery, 534120, product1); //сейчас мы как-то используем object?
-            var order2 = new Order<PickPointDelivery, object>(pickPointDelivery, 534121, product2);
-            var order3 = new Order<ShopDelivery, object>(shopDelivery, 534122, product3);
+            var order1 = new Order<HomeDelivery, List<Product>>(homeDelivery, 534120, new List<Product> {product1, product2 });
+            var order2 = new Order<PickPointDelivery, List<Product>>(pickPointDelivery, 534121, new List<Product> { product3 });
+            var order3 = new Order<ShopDelivery, List<Product>>(shopDelivery, 534122, new List<Product> { product1, product1});
 
             order1.OrderInfo();
             order1.DeliveryProcess();
@@ -32,6 +34,16 @@ namespace Final_project_class_1
 
             order3.OrderInfo();
             order3.DeliveryProcess();
+            Console.WriteLine();
+
+            order1.AddProduct(new Product("Pencil", 4));
+            order1.OrderInfo();
+            Console.WriteLine();
+
+            order2.RemoveProduct(product2);
+            order2.OrderInfo();
+            Console.WriteLine();
+
             Console.ReadKey();
 
         }
@@ -40,8 +52,30 @@ namespace Final_project_class_1
     //класс для описания товара
     public class Product
     {
-        public string Name;
-        public int Quantity;
+        private string _name;
+        private int _quantity;
+
+        public string Name
+        {
+            get => _name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Product name can not be null or empty.");
+                _name = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get => _quantity;
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Quantity can not be negative");
+                _quantity = value;
+            }
+        }
 
         public Product(string name, int quantity)
         {
@@ -49,13 +83,22 @@ namespace Final_project_class_1
             Quantity = quantity;
         }
 
+        public static Product operator +(Product product1, Product product2) //перегрузка оператора сложения для объединения продуктов
+        {
+            if (product1.Name != product2.Name)
+                throw new InvalidOperationException("Can not combine products with different names");
+
+            return new Product(product1.Name, product1.Quantity + product2.Quantity);
+        }
+
+
     }
 
     public class RecipientInfo
     {
-        public string LastName; //почему мы все-таки убрали авто свойства?
-        public string FirstName;
-        public string PhoneNumber;
+        public string LastName { get; private set; }
+        public string FirstName { get; private set; }
+        public string PhoneNumber { get; private set; }
 
         public RecipientInfo(string lastName, string firstName, string phoneNumber)
         {
@@ -68,10 +111,10 @@ namespace Final_project_class_1
 
     public abstract class Delivery
     {
-        public string Address;
-        public DateTime DeliveryTime;
-        public DayOfWeek DeliveryDayOfWeek;
-        public RecipientInfo Recipient;
+        public string Address { get; protected set; }
+        public DateTime DeliveryTime { get; protected set; }
+        public DayOfWeek DeliveryDayOfWeek { get; protected set; }
+        public RecipientInfo Recipient { get; protected set; }
 
         public Delivery(string address, RecipientInfo recipient, DayOfWeek deliveryDay, DateTime deliveryTime)
         {
@@ -88,7 +131,7 @@ namespace Final_project_class_1
 
     public class HomeDelivery : Delivery
     {
-        public string CourierName { get; set; } // а тут вот ставим свойство?
+        public string CourierName { get; private set; }
 
         public HomeDelivery(string address, RecipientInfo recipient, string courierName, DayOfWeek deliveryDay, DateTime deliveryTime) //конструктор
             : base(address, recipient, deliveryDay, deliveryTime)
@@ -105,10 +148,10 @@ namespace Final_project_class_1
 
     public class PickPointDelivery : Delivery
     {
-        public string WorkingHours;
+        public string WorkingHours { get; private set; }
         public PickPointDelivery(string address, RecipientInfo recipient, DayOfWeek deliveryDay, DateTime deliveryTime, string workingHours)
              : base(address, recipient, deliveryDay, deliveryTime)
-        {     
+        {
             WorkingHours = workingHours;
         }
 
@@ -116,46 +159,101 @@ namespace Final_project_class_1
         {
             Console.WriteLine($"PickPoint Delivery: {Address}, Working Hours: {WorkingHours}, Delivery Time: {DeliveryTime}");
         }
+    }
 
-        public class ShopDelivery : Delivery
+    public class ShopDelivery : Delivery
+    {
+        public string WorkingHours { get; private set; }
+
+        public ShopDelivery(string address, RecipientInfo recipient, DayOfWeek deliveryDay, DateTime deliveryTime, string workingHours)
+            : base(address, recipient, deliveryDay, deliveryTime)
         {
-            public string WorkingHours;
+            WorkingHours = workingHours;
+        }
 
-            public ShopDelivery(string address, RecipientInfo recipient, DayOfWeek deliveryDay, DateTime deliveryTime, string workingHours)
-                : base(address, recipient, deliveryDay, deliveryTime)
-            {
-                WorkingHours = workingHours;
-            }
+        public override void DeliveryInfo()
+        {
+            Console.WriteLine($"Shop Delivery: {Address}, Working Hours: {WorkingHours}, Delivery Time: {DeliveryTime}");
+        }
+    }
 
-            public override void DeliveryInfo()
+    public class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery { get; private set; }
+        public int Number { get; private set; }
+        public TStruct Products { get; private set; }
+
+        public Order(TDelivery delivery, int number, TStruct products)
+        {
+            Delivery = delivery;
+            Number = number;
+            Products = products;
+        }
+
+        public void OrderInfo()
+        {
+            Console.WriteLine($"Order {Number}: {Delivery.Address}. About delivery: ");
+            if (Products is List<Product> productList)
             {
-                Console.WriteLine($"Shop Delivery: {Address}, Working Hours: {WorkingHours}, Delivery Time: {DeliveryTime}");
+                foreach (var product in productList)
+                {
+                    Console.WriteLine($"Product - {product.Name}, Quantity  - {product.Quantity}");
+                }
             }
         }
 
-        public class Order<TDelivery, TStruct> where TDelivery : Delivery
+        public void DeliveryProcess()
         {
-            public TDelivery Delivery;
-            public int Number;
-            public Product Product;
+            Console.WriteLine($"Processing delivery for order {Number}...");
+            Delivery.DeliveryInfo();
+        }
 
-            public Order(TDelivery delivery, int number, Product product)
+        public Product this[int index]
+        {
+            get
             {
-                Delivery = delivery;
-                Number = number;
-                Product = product;
+                if (Products is List<Product> productsList)
+                {
+                    return productsList[index];
+                }
+                throw new InvalidOperationException("Product is not a List<Product>.");
             }
 
-            public void OrderInfo()
+            set
             {
-                Console.WriteLine($"Order {Number}: {Delivery.Address}. About delivery: {Product.Name}, Quantity: {Product.Quantity}");
+                if (Products is List<Product> productsList)
+                {
+                    productsList[index] = value;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Product is not a List<Product>.");
+                }
             }
+        }
 
-            public void DeliveryProcess()
+        public void AddProduct(Product product) //обобщенные методы
+        {
+            if (Products is List<Product> productsList)
             {
-                Console.WriteLine($"Processing delivery for order {Number}...");
-                Delivery.DeliveryInfo();
+                productsList.Add(product);
+            }
+            else
+            {
+                throw new InvalidOperationException("Product is not a List<Product>.");
+            }
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (Products is List<Product> productsList)
+            {
+                productsList.Remove(product);
+            }
+            else
+            {
+                throw new InvalidOperationException("Product is not a List<Product>.");
             }
         }
     }
-}
+}   
